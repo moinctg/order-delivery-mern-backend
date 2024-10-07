35cb2cd9d0c1818b6dd5d8a7ec5667c8f9ca2f39
Md Moinuddin Kamal <cpimoinuddin@gmail.com>
2024-10-07 16:23:09 +0600

    port

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import orderRouter from './routes/orderRoute.js';
 
 //app config
 const app = express()
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json())
