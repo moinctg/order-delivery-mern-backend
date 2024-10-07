7580a5ec7556bf041ef47883fcc8ad2ca5d9d01c
Md Moinuddin Kamal <cpimoinuddin@gmail.com>
2024-10-07 16:15:26 +0600

    update port

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,14 @@ import cors from 'cors'
 import { connectDB } from './config/db.js'
 import foodRouter from './routes/foodRoute.js'
 import userRouter from './routes/userRoute.js';
+
 // import 'dotenv/config';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
 
 //app config
 const app = express()
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 8000;
 
 // middleware
 app.use(express.json())
